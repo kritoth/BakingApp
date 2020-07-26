9325996f9fa51b8dd0b8ad84f8209a2b95ebfc68
kritoth <kritoth@gmail.com>
2020-07-26 18:53:06 +0200

    Create Database

---
+++ app/src/main/java/com/tiansirk/bakingapp/data/AppDatabase.java
@@ -0,0 +1,41 @@
+package com.tiansirk.bakingapp.data;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+
+@Database(entities = {Recipe.class, Ingredient.class, Step.class}, version = 1, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static final String TAG = AppDatabase.class.getSimpleName();
+    private static final Object LOCK = new Object();
+    private static final String DATABASE_NAME = "favoriterecipes";
+    private static AppDatabase sInstance;
+
+    public static AppDatabase getsInstance(Context context){
+        if(sInstance == null) {
+            synchronized (LOCK) {
+                if (sInstance == null) {
+
+                    Log.d(TAG, "Instantiating a new database");
+                    sInstance = Room.databaseBuilder(
+                            context.getApplicationContext(),
+                            AppDatabase.class,
+                            AppDatabase.DATABASE_NAME)
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        Log.d(TAG, "Returning the database instance already available");
+        return sInstance;
+    }
+
+    public abstract RecipeDao recipeDao();
+    public abstract IngredientDao ingredientDao();
+    public abstract StepDao stepDao();
+}
